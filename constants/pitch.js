0aa3ba76194ece75ef81e1ca54cfa24633cbecde
REFAP <jmantignac@ecoturbo.fr>
2025-09-10 07:12:28 +0200

    Update pitch.js

---
+++ constants/pitch.js
@@ -0,0 +1,15 @@
+// constants/pitch.js
+export const LINKS = {
+  carterCash: 'https://auto.re-fap.fr/?utm_source=autoai&utm_medium=cta&utm_campaign=v2',
+  garage: 'https://re-fap.fr/trouver_garage_partenaire/?utm_source=autoai&utm_medium=cta&utm_campaign=v2'
+};
+
+export const FAP_BENEFITS = [
+  '**Économique** : évite le remplacement (dès **99€ TTC** en Carter-Cash).',
+  '**Éco-responsable** : on réutilise la pièce, moins de déchets.',
+  '**Résultat ≈ neuf** si le FAP n’est pas endommagé (performances restaurées).'
+];
+
+export const LEAD_GARAGE_SNIPPET =
+  'Garage de **confiance** près de chez toi : saisis **immatriculation** + **code postal**, ' +
+  'reçois un **devis de diagnostic au meilleur prix** et **prends RDV rapidement**.';
